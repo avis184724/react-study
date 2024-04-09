37fda297ce17aab75aa52aacfe52fc869287571d
avis184724 <ckdtjqdld9632@naver.com>
2024-04-09 21:17:23 +0900

    2024_04_09

---
+++ 4_09/main2.js
@@ -0,0 +1,17 @@
+const boxEL = document.querySelector('.box');
+boxEL.addEventListener('click',function() {
+    console.log('clicked');
+    boxEL.classList.add('active');
+
+    let hasActive = boxEL.classList.contains('active');
+    console.log(hasActive);
+    boxEL.classList.remove('active');
+    hasActive = boxEL.classList.contains('active');
+    console.log(hasActive);
+});
+console.log(boxEL)
+
+const boxELs = document.querySelector('box');
+boxELs.forEach(function (bocEL, index) {
+    console.log(boxEL, index)
+});
