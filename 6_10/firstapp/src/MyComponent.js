2d72050b13d1dc31e66a95acb440161504cf3b67
avis184724 <ckdtjqdld9632@naver.com>
2024-06-10 21:09:11 +0900

    6_10

--- 6_10/firstapp/src/MyComponent.js
+++ 6_10/firstapp/src/MyComponent.js
@@ -17,18 +17,18 @@ import { Component } from 'react';
 // }
 
 // 클래스형 컴포넌트
-class MyComponent extends Component {
-    render() {
-        const {name, number, children} = this.props;
-        return (<div>안녕하세요 제 이름은 {name} 입니다. <br/>
-        children 값은 {children} 입니다. <br/>
-        제가 좋아하는 숫자는 {number} 입니다.
-        </div>)
-    }
-}
-MyComponent.defaultProps = {
-    name : "기본 이름"
-};
+// class MyComponent extends Component {
+//     render() {
+//         const {name, number, children} = this.props;
+//         return (<div>안녕하세요 제 이름은 {name} 입니다. <br/>
+//         children 값은 {children} 입니다. <br/>
+//         제가 좋아하는 숫자는 {number} 입니다.
+//         </div>)
+//     }
+// }
+// MyComponent.defaultProps = {
+//     name : "기본 이름"
+// };
 // 프롭타임으로 검사
 MyComponent.PropTypes = {
     name : PropTypes.string,
