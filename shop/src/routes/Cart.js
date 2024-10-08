923fbe5adcfb665f028669dabd68d93867fc58ba
avis184724 <ckdtjqdld9632@naver.com>
2024-10-08 20:08:49 +0900

    10_08

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,13 +1,14 @@
 import { Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeName } from '../store';
+import { changeName, addAge, addCount, decreaseCount } from '../store';
 
 function Cart() {
   let state = useSelector((state) => state)
   let dispatch = useDispatch()
   return (
     <div>
-      {state.user}의 장바구니
+      {state.user.name}의 장바구니
+      {state.user.age}의 나이
       <Table>
         <thead>
           <tr>
@@ -26,8 +27,14 @@ function Cart() {
                 <td>{state.cart[i].count}</td>
                 <td>
                   <button onClick={()=> {
-                    dispatch(changeName())
+                    dispatch(changeName('jack'))
+                    dispatch(addCount(i))
                   }}>+</button>
+                </td>
+                <td>
+                  <button onClick={()=> {
+                    dispatch(decreaseCount(i))
+                  }}>-</button>
                 </td>
               </tr>
         
--- shop/src/store.js
+++ shop/src/store.js
@@ -2,15 +2,16 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 let user = createSlice({
   name : 'user',
-  initialState : 'kim',
+  initialState : { name : 'kim', age : 20},
   reducers : {
-    changeName(state) {
-      return 'john' + state
+    changeName(state, action) {
+      state.name = action.payload
+    },
+    addAge(state, action) {
+      state.age += action.payload 
     }
   }
 })
-
-export let {changeName} = user.actions
 
 let stock = createSlice({
   name : 'stock',
@@ -20,9 +21,19 @@ let stock = createSlice({
 let cart = createSlice({
   name : 'cart',
   initialState : [
-    {id : 0, name : 'white and black', count : 2},
-    {id : 2, name : 'Gray Yordan', count : 1}
-  ]
+    {id : 0, name : 'white and black', count : 2, default_count : 2},
+    {id : 2, name : 'Gray Yordan', count : 1, default_count : 1}
+  ],
+  reducers : {
+    addCount(state, action) {
+      state[action.payload].count++
+    },
+    decreaseCount(state, action) {
+      if (state[action.payload].count > state[action.payload].default_count) {
+        state[action.payload].count--
+      }
+    }
+  }
 })
 
 export default configureStore({
@@ -32,3 +43,6 @@ export default configureStore({
     cart : cart.reducer
   }
 })
+
+export let {changeName, addAge} = user.actions
+export let {addCount, decreaseCount} = cart.actions
